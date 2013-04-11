25d59885ffbd4b86574b769b6d5212927d8c19c7
Saúl Eduardo López Cabrera <fimbul_invierno_eterno@hotmail.com>
2013-04-11 09:52:31 -0500

    Proyecto para probar las funcionalidades agregadas
    Proyecto para verificar el uso del panel y de paneles hijos, junto la
    implementación de un par de eventos.

---
+++ GameEngineTests/GameEngineTest.UITests/GameEngineTest.UITests/MainGameScene.cs
@@ -0,0 +1,35 @@
+﻿using FarseerPhysics.Dynamics;
+using GameEngine;
+using GameEngine.Gaming;
+using GameEngine.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameEngineTest.UITests
+{
+    class MainGameScene : Scene
+    {
+        public MainGameScene()
+            : base(new World(Vector2.Zero))
+        {
+            var handler = new InputHandler();
+            handler.CursorSprite = new Sprite("Mouse");
+
+            Panel panelPadre = new Panel(handler, new FullColorSprite(500, 500, Color.WhiteSmoke));
+            panelPadre.Hover += (o, e) => { panelPadre.Image.Color = Color.Wheat; };
+            panelPadre.UnHover += (o, e) => { panelPadre.Image.Color = Color.WhiteSmoke; };
+
+            Panel panelHijo = new Panel(handler, new FullColorSprite(50, 50, Color.Black));
+            panelHijo.Parent = panelPadre;
+            panelHijo.Position = new Vector2(30, 30);
+
+            panelPadre.Position = new Vector2(200, 200);
+            Add(panelPadre);
+            Add(handler);
+        }
+    }
+}
---
+++ GameEngineTests/GameEngineTest.UITests/GameEngineTest.UITests/MainGameSceneManager.cs
@@ -0,0 +1,27 @@
+﻿using GameEngine.Gaming;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameEngineTest.UITests
+{
+    public class MainGameSceneManager : GameSceneManager
+    {
+        MainGameScene scene;
+        public MainGameSceneManager(Game game) : base(game)
+        {
+            scene = new MainGameScene();
+            Add("panel", scene);
+            ChangeState("panel");
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch.Begin();
+            base.Draw(gameTime);
+            SpriteBatch.End();
+        }
+    }
+}
